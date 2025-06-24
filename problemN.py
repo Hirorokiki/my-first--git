6d1330cc851d979502aa5ad7d93ae957e7702d2d
Hiroki Kasuya <h.kasuya.315@stn.nitech.ac.jp>
2025-06-24 15:04:03 +0900

    日本語化

--- problemN.py
+++ problemN.py
@@ -1,32 +1,26 @@
-import collections
-
-L = input()
-l = sorted(L)
-
-if len(l) < 2:
+L=input()
+l=sorted(L)
+A=[]
+a=0
+if len(l)==1:
     print("いいえ")
     exit()
-
-counts = []
-start_index = 0
-for i in range(1, len(l)):
-    if l[i] != l[i-1]:
-        counts.append(i - start_index)
-        start_index = i
-
-counts.append(len(l) - start_index)
-
-freq = collections.Counter(counts)
-
-result = "はい"
-for f in freq.values():
-    if f != 2:
-        result = "いいえ"
-        break
-
-print(result)
-
-
-
-
-
+if len(l)>=2:
+    for i in range(1,len(l)):
+        if l[i]!=l[i-1]:
+            A.append(i-a)
+            a=i
+    if i==len(l)-1:
+        A.append(len(l)-a)
+for i in range(0,len(A)):
+    count=1
+    for j in range(0,len(A)):
+        if A[i]==A[j] and i!=j:
+            count=count+1
+        if count>=3:
+        print("いいえ")
+        exit()
+    if count==1:
+        print("いいえ")
+        exit()
+print("はい")
